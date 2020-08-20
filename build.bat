6c8af73afe42953078cf583820f916df71ab2646
jockus <joakim.hentula@gmail.com>
2020-08-20 16:01:08 +0100

    Sort out assimp building, remove temporaries

--- build.bat
+++ build.bat
@@ -1,25 +1,28 @@
-rmdir /S /Q assimp
-git clone https://github.com/assimp/assimp.git/tree/v4.1.0
+rem rmdir /S /Q assimp
+rem git clone -b v4.1.0 https://github.com/assimp/assimp
 
-rmdir /S /Q include
-mkdir include
+rem rmdir /S /Q include
+rem mkdir include
 
-rmdir /S /Q lib
-mkdir lib
+rem rmdir /S /Q lib
+rem mkdir lib
+		 
+		 rem -DRT_LIBRARY=msvcrt.lib^
+		 rem -DBUILD_SHARED_LIBS=OFF^
 
 pushd assimp
 	mkdir build
 	pushd build
 		cmake -G "NMake Makefiles"^
-		 -DASSIMP_NO_EXPORT=OFF^
+		 -DASSIMP_NO_EXPORT=ON^
 		 -DASSIMP_INSTALL_PDB=OFF^
 		 -DASSIMP_BUILD_ASSIMP_TOOLS=OFF^
 		 -DASSIMP_BUILD_TESTS=OFF^
+		 -DASSIMP_BUILD_SAMPLES=OFF^
 		 -DASSIMP_BUILD_ALL_IMPORTERS_BY_DEFAULT=OFF^
 		 -DASSIMP_BUILD_FBX_IMPORTER=ON^
 		 -DASSIMP_BUILD_OBJ_IMPORTER=ON^
 		 -DCMAKE_BUILD_TYPE=Release^
-		 -DASSIMP_BUILD_STATIC_LIB=ON^
 		 ..
 		nmake
 		copy code\*.lib ..\..\
